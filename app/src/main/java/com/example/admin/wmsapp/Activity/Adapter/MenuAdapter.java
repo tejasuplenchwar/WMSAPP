8408132fd2bed0be3edb9285b13c9f678a0aa9bd
tejas <tejas.uplenchwar@adnatesolutions.com>
2017-04-04 16:20:29 +0530

    new Commit after menutab add

--- app/src/main/java/com/example/admin/wmsapp/Activity/Adapter/MenuAdapter.java
+++ app/src/main/java/com/example/admin/wmsapp/Activity/Adapter/MenuAdapter.java
@@ -1,8 +1,65 @@
 package com.example.admin.wmsapp.Activity.Adapter;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.admin.wmsapp.Activity.Model.Menu;
+import com.example.admin.wmsapp.R;
+
+import java.util.ArrayList;
+
 /**
  * Created by admin on 04/04/2017.
  */
 
-public class MenuAdapter  {
+public class MenuAdapter extends ArrayAdapter<Menu> {
+    private final Context context;
+    private final ArrayList<Menu> menuList;
+
+    public MenuAdapter(Context context,ArrayList<Menu> menuList) {
+        super(context, 0);
+        this.context=context;
+        this.menuList=menuList;
+    }
+
+    @Override
+    public int getCount() {
+        return menuList.size();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Menu menu=menuList.get(position);
+        LinearLayout layout=null;
+        if(convertView==null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            layout = (LinearLayout) layoutInflater.inflate(R.layout.menu_list_item, null);
+        }
+        else
+        {
+            layout=(LinearLayout)convertView;
+        }
+
+        if ((position % 2) == 0) {
+            layout.setBackgroundColor(Color.parseColor("#f6f6f5"));
+        } else {
+            layout.setBackgroundColor(Color.parseColor("#fbfbfb"));
+        }
+        TextView textMenuId= (TextView) layout.findViewById(R.id.textMenuId);
+        TextView textMenuName= (TextView) layout.findViewById(R.id.textMenuName);
+        TextView textMenuIcon= (TextView) layout.findViewById(R.id.textMenuIcon);
+
+        textMenuId.setText("Menu-Id"+menu.getMenu_id());
+        textMenuName.setText(menu.getMenu_name());
+        textMenuIcon.setText(menu.getMenu_icon());
+        return layout;
+    }
 }
--- app/src/main/java/com/example/admin/wmsapp/Activity/tabs/Menu.java
+++ app/src/main/java/com/example/admin/wmsapp/Activity/tabs/Menu.java
@@ -6,29 +6,82 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.admin.wmsapp.Activity.Adapter.MenuAdapter;
+import com.example.admin.wmsapp.Activity.Services.MenuAPI;
 import com.example.admin.wmsapp.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.admin.wmsapp.Activity.Model.Constants.ROOT_URL;
 
 /**
  * Created by admin on 04/04/2017.
  */
 
 public class Menu extends Fragment {
+    ListView listView;
+    ArrayList<com.example.admin.wmsapp.Activity.Model.Menu> menuList=new ArrayList<>();
+    MenuAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.menu, container, false);
+        getAllMenu();
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    private void getAllMenu(){
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(ROOT_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                 .build();
+
+        MenuAPI api=retrofit.create(MenuAPI.class);
+        Call<ArrayList<com.example.admin.wmsapp.Activity.Model.Menu>> call=api.getAllMenu();
+        call.enqueue(new Callback<ArrayList<com.example.admin.wmsapp.Activity.Model.Menu>>() {
+            @Override
+            public void onResponse(Call<ArrayList<com.example.admin.wmsapp.Activity.Model.Menu>> call, Response<ArrayList<com.example.admin.wmsapp.Activity.Model.Menu>> response) {
+
+
+                if(response.body()!=null){
+                    menuList=response.body();
+                     showList();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<com.example.admin.wmsapp.Activity.Model.Menu>> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private void showList(){
+        Collections.reverse(menuList);
+        adapter=new MenuAdapter(getActivity(),menuList);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
